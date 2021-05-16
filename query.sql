84a9fbf78fe53aed05ec0bbea677aef212a10193
Syreeta Berry <mzberry1@hotmail.com>
2021-05-16 04:07:11 -0400

    two done

--- query.sql
+++ query.sql
@@ -2,6 +2,9 @@
 
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' AND hire_date<= '1986-12-31';
 
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
@@ -10,6 +13,9 @@
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name = 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
