eadebc9a9b2e5f833e52ea63fdf50a255cf108a1
Syreeta Berry <mzberry1@hotmail.com>
2021-05-16 15:21:06 -0400

    six done

--- query.sql
+++ query.sql
@@ -11,6 +11,13 @@ WHERE hire_date >= '1986-01-01' AND hire_date<= '1986-12-31';
 
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name,          
+       d.dept_no, d.dept_name                            
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON d.dept_no = de.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
@@ -18,7 +25,27 @@ FROM employees
 WHERE first_name = 'Hercules' AND last_name = 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name,          
+       d.dept_no, d.dept_name                            
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name,          
+       d.dept_no, d.dept_name                            
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(first_name) AS "Frenquency"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frenquency" DESC;
