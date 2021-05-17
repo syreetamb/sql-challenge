928bfd4b55bc6cf9cdc5a29e4cd2c55e711e7890
Syreeta Berry <mzberry1@hotmail.com>
2021-05-16 20:36:37 -0400

    eight done

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,4 +1,9 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name,          
+       e.sex, s.salary                             
+FROM employees as e
+INNER JOIN salaries as s
+ON e.emp_no = s.emp_no;
 
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
@@ -8,6 +13,13 @@ WHERE hire_date >= '1986-01-01' AND hire_date<= '1986-12-31';
 
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT e.emp_no, e.last_name, e.first_name,          
+       d.dept_no, d.dept_name                             
+FROM employees as e
+INNER JOIN dept_manager as dm
+ON e.emp_no = dm.emp_no
+INNER JOIN departments as d
+ON d.dept_no = dm.dept_no;
 
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
